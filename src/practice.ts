a2d2115b963fdc2b76ddfa8bc0e3d29ab5435654
lazy-sky <1cielo@naver.com>
2022-01-06 20:50:01 +0900

    generics

--- src/practice.ts
+++ src/practice.ts
@@ -1,32 +1,32 @@
-// Type Alias
-// type 키워드를 통해 특정 타입에 별칭을 붙일 수 있다.
+// Generics
 
-type Person = {
-  name: string;
-  age?: number;
-};
+// 어떤 타입이 올 지 모르는 경우엔 any 타입을 사용할 수 있다.
+// 하지만 이렇게 하면 타입 추론이 모두 깨진 거나 다름없다.
+function mergeDirtyVersion(a: any, b: any): any {
+  return {
+    ...a,
+    ...b,
+  };
+}
 
-// &는 두 개 이상의 타입을 합쳐준다.
-type Developer = Person & {
-  skills: string[];
-};
+const merged = mergeDirtyVersion({ foo: 1 }, { bar: 1 });
 
-const person = {
-  name: "김사람",
-};
+// 이런 상황에 제네릭을 사용하면 된다.
 
-const expert: Developer = {
-  name: "김개발",
-  skills: ["javascript", "react"],
-};
+// 꺽쇠 안에 타입의 이름을 넣어주면 제네릭에 해당하는 타입에는 뭐든지 들어올 수 있게 되는 한편,
+// 사용할 때 타입이 깨지지 않게 된다.
+// 아래와 같이 함수에 제네릭을 사용하게 되면 함수의 파라미터로 넣은 실제 값의 타입을 활용하게 된다.
+function merge<A, B>(a: A, b: B): A & B {
+  return {
+    ...a,
+    ...b,
+  };
+}
 
-type People = Person[]; // Person[]를 People 타입으로 사용할 수 있게 되었다.
-const people: People = [person, expert];
+function wrap<T>(param: T) {
+  return {
+    param,
+  };
+}
 
-type Color = "red" | "orange" | "yellow";
-const color: Color = "red";
-const colors: Color[] = ["red", "orange"];
-
-// type과 interface에 많은 차이가 있는 것은 아니다.
-// 무엇이든 써도 되지만 일관성을 지켜야 한다.
-// 다만 라이브러리를 작성하고 다른 라이브러리를 위한 타입 지원 파일을 작성하게 될 때는 interface가 권장된다.
+const wrapped = wrap(10);
