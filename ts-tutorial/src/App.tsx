e411e0451d5be383c55354d34060bd0769dd39ad
lazy-sky <1cielo@naver.com>
2022-01-06 22:32:01 +0900

    Counter

--- ts-tutorial/src/App.tsx
+++ ts-tutorial/src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import Greetings from './Greetings';
+import Counter from './Counter';
 
 const App: React.FC = () => {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
-  };
-
-  // 컴포넌트를 렌더링할 때 필요한 props를 빠뜨리게 되면 에디터에 오류가 나타날 것이다.
-  // 컴포넌트를 사용하는 과정에서 어떤 게 필요했는지 기억이 나지 않을 땐,
-  // 커서를 컴포넌트 위에 올려보거나,
-  // 컴포넌트의 props를 설정하는 부분에서 Ctrl + Space를 눌러보면 된다.
-  return <Greetings name="Sky" onClick={onClick} />;
+  return <Counter />;
 };
 
 export default App;
