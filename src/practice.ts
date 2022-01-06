bd2756d2b4303a80fbc610b9a635e47a8a4479e8
lazy-sky <1cielo@naver.com>
2022-01-06 18:29:36 +0900

    일반적인 객체

--- src/practice.ts
+++ src/practice.ts
@@ -33,4 +33,24 @@ class Rectangle implements Shape {
 
 const shape: Shape[] = [new Circle(5), new Rectangle(10, 5)];
 
-console.log(shape);
+// 일반 객체를 interface로 타입 설정하기
+
+interface Person {
+  name: string;
+  age?: number; // ?는 옵셔널 값이라는 의미.
+}
+interface Developer {
+  name: string;
+  age?: number;
+  skills: string[];
+}
+
+const person: Person = {
+  name: "김사람",
+  age: 20,
+};
+
+const expert: Developer = {
+  name: "김개발",
+  skills: ["javascript", "react"],
+};
