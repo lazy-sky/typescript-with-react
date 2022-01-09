5eed7ed95b02f29b31b315ec265dec5c17659bb7
lazy-sky <1cielo@naver.com>
2022-01-09 18:21:39 +0900

    디스패치 전용 컨텍스트

--- ts-tutorial/src/context/TodosContext.tsx
+++ ts-tutorial/src/context/TodosContext.tsx
@@ -5,7 +5,7 @@
 // TodoForm 컴포넌트처럼 상태는 필요하지 않고, 디스패치 함수만 필요한 컴포넌트도
 // 상태가 업데이트될 때 리렌더링하게 된다.
 
-import { createContext } from 'react';
+import { createContext, Dispatch } from 'react';
 
 export type Todo = {
   id: number;
@@ -19,3 +19,15 @@ type TodoState = Todo[];
 // 컨텍스트를 만들 땐 아래와 같이 제네릭을 사용하여 컨텍스트에서 관리할 값의 상태를 설정할 수 있다.
 // 추후 Provider를 사용하지 않았을 때는 컨텍스트의 값이 undefined가 되야하므로, 아래와 같이 선언한다.
 const TodosStateContext = createContext<TodoState | undefined>(undefined);
+
+type Action =
+  | { type: 'CREATE'; text: string }
+  | { type: 'TOGGLE'; id: number }
+  | { type: 'REMOVE'; id: number };
+
+// 이렇게 Dispatch를 리액트 패키지에서 불러와 제네릭으로 액션들의 타입을 넣어주면
+// 추후 컴포넌트에서 액션을 디스패치할 때 액션들에 대한 타입을 검사할 수 있다.
+type TodosDispatch = Dispatch<Action>;
+const TodosDispatchContext = createContext<TodosDispatch | undefined>(
+  undefined,
+);
