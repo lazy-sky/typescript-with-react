5859df16b95062461adca21fb6a9d397227dc12c
lazy-sky <1cielo@naver.com>
2022-01-10 00:01:29 +0900

    rootReducer

---
+++ ts-redux-tutorial/src/modules/index.ts
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux";
+import counter from "./counter";
+
+const rootReducer = combineReducers({
+  counter,
+});
+
+export default rootReducer;
+
+// 루트 리듀서를 만드는 방법인 일반 JS 환경과 같은데 주의할 점은,
+// RootState 타입을 만들어 내보내줘야 한다는 것이다.
+// 이 타입은 추후 컨테이너 컴포넌트를 만들게 될 때 스토어에서 관리하고 있는 상태를 조회하기 위해 useSelector를 사용할 떄 필요하다.
+export type RootState = ReturnType<typeof rootReducer>;
