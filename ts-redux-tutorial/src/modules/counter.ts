85f800a1813897f0df8601017362b2f311ef037c
lazy-sky <1cielo@naver.com>
2022-01-10 21:59:44 +0900

    refact: counter 리듀서 메소드 체이닝 방식으로 구현

--- ts-redux-tutorial/src/modules/counter.ts
+++ ts-redux-tutorial/src/modules/counter.ts
@@ -37,10 +37,46 @@ const initialState: CounterState = {
 // 리듀서
 // typesafe-actions의 createReducer를 사용하면 리듀서를 switch/case문이 아닌 object map 형태로 구현할 수 있다.
 // 제네릭으로 상태의 타입과 액션들의 타입을 넣어줘야 한다.
-const counter = createReducer<CounterState, CounterAction>(initialState, {
-  [INCREASE]: (state) => ({ count: state.count + 1 }),
-  [DECREASE]: (state) => ({ count: state.count - 1 }),
-  [INCREASE_BY]: (state, action) => ({ count: state.count + action.payload }),
-});
+// const counter = createReducer<CounterState, CounterAction>(initialState, {
+//   [INCREASE]: (state) => ({ count: state.count + 1 }),
+//   [DECREASE]: (state) => ({ count: state.count - 1 }),
+//   [INCREASE_BY]: (state, action) => ({ count: state.count + action.payload }),
+// });
+
+// 메소드 체이닝 방식으로도 구현할 수 있다.
+// handleAction의 파라미터는 각각 액션의 타입, 업데이터 함수다.
+const counter = createReducer<CounterState, CounterAction>(initialState)
+  .handleAction(increase, (state) => ({ count: state.count + 1 }))
+  .handleAction(decrease, (state) => ({ count: state.count - 1 }))
+  .handleAction(increaseBy, (state, action) => ({
+    count: state.count + action.payload,
+  }));
 
 export default counter;
+
+// 첫 번째 파라미터로 액션의 문자열 타입 대신 액션 생성 함수를 넣어주면
+// 액션들의 문자열 타입과 액션들의 객체 타입을 생략하여 작성할 수도 있다.
+// object map 방식보다 가독성은 다소 떨어지지만 생략할 수 있는 게 많다.
+// 액션 생성 함수를 넣어도 잘 작동하는 이유는 createAction을 통해서 만드는 액션 생성 함수가 getType 함수를 지니고 있기 때문이다.
+/* e.g.,
+export const increase = createStandardAction('counter/INCREASE')();
+export const decrease = createStandardAction('counter/DECREASE')();
+export const increaseBy = createStandardAction('counter/INCREASE_BY')<number>(); // payload 타입을 Generics 로 설정해주세요.
+
+type CounterState = {
+  count: number;
+};
+
+const initialState: CounterState = {
+  count: 0
+};
+
+const counter = createReducer(initialState)
+  .handleAction(increase, state => ({ count: state.count + 1 }))
+  .handleAction(decrease, state => ({ count: state.count - 1 }))
+  .handleAction(increaseBy, (state, action) => ({
+    count: state.count + action.payload
+  }));
+
+export default counter;
+*/
